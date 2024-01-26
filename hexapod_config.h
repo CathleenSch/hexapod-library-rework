783dcbfd086d99c0120568101710fcd00de127f8
casch <ca.schmalfuss@live.de>
2024-01-26 16:48:09 +0100

    Add gitignore; extract config

---
+++ hexapod_config.h
@@ -0,0 +1,10 @@
+#ifndef HEXAPOD_CONFIG_H
+#define HEXAPOD_CONFIG_H
+
+int walkingSpeed = 50;
+int threatenHeadMovementSpeed = 15;
+
+const int pingPin = A3;
+const int trigPin = A2;
+
+#endif // !HEXAPOD_CONFIG_H
